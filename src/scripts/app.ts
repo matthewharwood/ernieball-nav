290bf6c69ffa605a249e88b9de72d212329798b7
Matthew Harwood <matthhar12@gmail.com>
2018-06-29 08:55:11 -0700

    - Added active state to primary navigation.

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -9,7 +9,8 @@ const DataAttrs = {
   DROPDOWN: "data-lang-dropdown",
   DROPDOWN_LIST: "data-lang-dropdown-list",
   LINK: "data-site-link",
-  OUTLET: "data-site-outlet"
+  OUTLET: "data-site-outlet",
+  ROOT: "data-site-root",
 };
 
 const EventTypes = {
@@ -18,50 +19,75 @@ const EventTypes = {
   MOUSELEAVE: "mouseleave"
 };
 
+// Locale Dropdown
 const dropdown = document.querySelector(`[${DataAttrs.DROPDOWN}]`) as HTMLElement;
-
 const dropdownList = document.querySelector(`[${DataAttrs.DROPDOWN_LIST}]`) as HTMLElement;
-dropdown.addEventListener(EventTypes.CLICK, () => {
-  dropdownList.classList.toggle(ClassNames.LANG_DROPDOWN_ACTIVE);
-});
 
-/*
- * Primary Nav Fly out.
- */
+const localeHandler = () => {
+  dropdownList.classList.toggle(ClassNames.LANG_DROPDOWN_ACTIVE);
+};
+dropdown.addEventListener(EventTypes.CLICK, localeHandler);
+
+
+// Primary Desktop Overlay
+const rootSiteLink = document.querySelector(`[${DataAttrs.ROOT}]`) as HTMLElement;
 const overlay = document.querySelector(`.${ClassNames.DESKTOP_NAV}`) as HTMLElement;
 const links = document.querySelectorAll(`[${DataAttrs.LINK}]`) as NodeListOf<HTMLElement>;
 const outlets = document.querySelectorAll(`[${DataAttrs.OUTLET}]`) as NodeListOf<HTMLElement>;
 
-type LinkOrOutlet = "outlet" | "link";
-const mapLinksOutlets = (ls: NodeListOf<HTMLElement>, os: NodeListOf<HTMLElement>): Map<string, Map<LinkOrOutlet, HTMLElement>> => {
+const mapLinksOutlets = (ls: NodeListOf<HTMLElement>, os: NodeListOf<HTMLElement>): Map<string, Map<string, HTMLElement>> => {
   const map = new Map();
-
   Array.from(ls).forEach(l => {
     Array.from(os).forEach(o => {
       if (o.attributes.getNamedItem(DataAttrs.OUTLET).value === l.attributes.getNamedItem(DataAttrs.LINK).value) {
         const val = l.attributes.getNamedItem(DataAttrs.LINK).value;
-        map.set(val, new Map().set("outlet", o).set("link", l));
+        map.set(val, new Map().set(DataAttrs.OUTLET, o).set(DataAttrs.LINK, l));
       }
     });
   });
   return map;
 };
+
+
 const linksOutletsMap = mapLinksOutlets(links, outlets);
+
 
 
 let timeout: any = null;
 
+const setRootsLinkValue = (id = '*') => {
+  rootSiteLink.setAttribute(DataAttrs.LINK, id);
+};
+
+const setActiveLink = (id = '*') => {
+  const linkId = parseInt(id, 10);
+
+  if(isNaN(linkId)) {
+    linksOutletsMap.forEach(l => l.get(DataAttrs.LINK).classList.remove(ClassNames.ACTIVE))
+  } else {
+    linksOutletsMap.get(id).get(DataAttrs.LINK).classList.add(ClassNames.ACTIVE);
+  }
+};
+
 const activate = (t) => {
-  // linksOutletsMap
-  linksOutletsMap.forEach((v) => v.get("outlet").classList.remove(ClassNames.ACTIVE));
-  linksOutletsMap.get(t.attributes.getNamedItem(DataAttrs.LINK).value).get("outlet").classList.add(ClassNames.ACTIVE);
+  const linkId = t.attributes.getNamedItem(DataAttrs.LINK).value;
+
+  linksOutletsMap.forEach((v) => v.get(DataAttrs.OUTLET).classList.remove(ClassNames.ACTIVE));
+  linksOutletsMap.get(linkId).get(DataAttrs.OUTLET).classList.add(ClassNames.ACTIVE);
+
+  setRootsLinkValue(linkId);
+  setActiveLink(linkId);
   overlay.classList.add(ClassNames.ACTIVE);
 };
 
 const deactivate = () => {
+  setRootsLinkValue();
+  setActiveLink();
   overlay.classList.remove(ClassNames.ACTIVE);
 };
 
+
+// HANDLERS
 const enterHandler = ({ target }: MouseEvent) => {
   activate(target);
   clearTimeout(timeout);
@@ -72,9 +98,18 @@ const leaveHandler = () => {
   timeout = setTimeout(() => deactivate(), DEBOUNCE_TIMER);
 };
 
-Array.from(links).forEach(l => {
-  l.addEventListener(EventTypes.MOUSEENTER, enterHandler);
-  l.addEventListener(EventTypes.MOUSELEAVE, leaveHandler);
-});
+// LISTENERS
+function main() {
+
+  Array.from(links).forEach(l => {
+    l.addEventListener(EventTypes.MOUSEENTER, enterHandler);
+    l.addEventListener(EventTypes.MOUSELEAVE, leaveHandler);
+  });
+}
+
+
+main();
+
+// TODO: make these classes;
 
 
