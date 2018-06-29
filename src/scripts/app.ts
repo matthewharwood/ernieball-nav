610e852cf913dcfd1311d7208ca5714a708b018a
Matthew Harwood <matthhar12@gmail.com>
2018-06-28 23:51:13 -0700

    - Added overlays for navigation items using map datastructure.

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,34 +1,39 @@
-const dropdown = document.querySelector("[data-lang-dropdown]") as HTMLElement;
+const DEBOUNCE_TIMER = 500;
+const ClassNames = {
+  ACTIVE: "active",
+  DESKTOP_NAV: "flyout__desktop-nav",
+  LANG_DROPDOWN_ACTIVE: "lang-select__dropdown--active"
+};
 
-const dropdownList = document.querySelector("[data-lang-dropdown-list]") as HTMLElement;
-dropdown.addEventListener("click", (e) => {
-  dropdownList.classList.toggle("lang-select__dropdown--active");
+const DataAttrs = {
+  DROPDOWN: "data-lang-dropdown",
+  DROPDOWN_LIST: "data-lang-dropdown-list",
+  LINK: "data-site-link",
+  OUTLET: "data-site-outlet"
+};
+
+const EventTypes = {
+  CLICK: "click",
+  MOUSEENTER: "mouseenter",
+  MOUSELEAVE: "mouseleave"
+};
+
+const dropdown = document.querySelector(`[${DataAttrs.DROPDOWN}]`) as HTMLElement;
+
+const dropdownList = document.querySelector(`[${DataAttrs.DROPDOWN_LIST}]`) as HTMLElement;
+dropdown.addEventListener(EventTypes.CLICK, () => {
+  dropdownList.classList.toggle(ClassNames.LANG_DROPDOWN_ACTIVE);
 });
 
 /*
  * Primary Nav Fly out.
  */
-const ClassNames = {
-  ACTIVE: "active",
-  DESKTOP_NAV: "flyout__desktop-nav"
-};
-
-const DataAttrs = {
-  LINK: "data-site-link",
-  OUTLET: "data-site-outlet"
-};
-
 const overlay = document.querySelector(`.${ClassNames.DESKTOP_NAV}`) as HTMLElement;
 const links = document.querySelectorAll(`[${DataAttrs.LINK}]`) as NodeListOf<HTMLElement>;
 const outlets = document.querySelectorAll(`[${DataAttrs.OUTLET}]`) as NodeListOf<HTMLElement>;
 
-interface ILinksOutletsMap {
-  elementOutlet: HTMLElement;
-  linksOutlet: HTMLElement;
-  id: number;
-}
-
-const mapLinksOutlets = (ls: NodeListOf<HTMLElement>, os: NodeListOf<HTMLElement>): Map<string, Map<string, HTMLElement>> => {
+type LinkOrOutlet = "outlet" | "link";
+const mapLinksOutlets = (ls: NodeListOf<HTMLElement>, os: NodeListOf<HTMLElement>): Map<string, Map<LinkOrOutlet, HTMLElement>> => {
   const map = new Map();
 
   Array.from(ls).forEach(l => {
@@ -42,35 +47,34 @@ const mapLinksOutlets = (ls: NodeListOf<HTMLElement>, os: NodeListOf<HTMLElement
   return map;
 };
 const linksOutletsMap = mapLinksOutlets(links, outlets);
-console.log(linksOutletsMap);
-
-
 
 
 let timeout: any = null;
 
-
-const addActive = () => {
+const activate = (t) => {
+  // linksOutletsMap
+  linksOutletsMap.forEach((v) => v.get("outlet").classList.remove(ClassNames.ACTIVE));
+  linksOutletsMap.get(t.attributes.getNamedItem(DataAttrs.LINK).value).get("outlet").classList.add(ClassNames.ACTIVE);
   overlay.classList.add(ClassNames.ACTIVE);
 };
 
-const removeActive = () => {
+const deactivate = () => {
   overlay.classList.remove(ClassNames.ACTIVE);
 };
 
-const showOverlayHandler = ({ target }: any) => {
-  addActive();
+const enterHandler = ({ target }: MouseEvent) => {
+  activate(target);
   clearTimeout(timeout);
 };
 
-const removeOverlayHandler = () => {
+const leaveHandler = () => {
   clearTimeout(timeout);
-  timeout = setTimeout(() => removeActive(), 500);
+  timeout = setTimeout(() => deactivate(), DEBOUNCE_TIMER);
 };
 
 Array.from(links).forEach(l => {
-  l.addEventListener("mouseenter", showOverlayHandler);
-  l.addEventListener("mouseleave", removeOverlayHandler);
+  l.addEventListener(EventTypes.MOUSEENTER, enterHandler);
+  l.addEventListener(EventTypes.MOUSELEAVE, leaveHandler);
 });
 
 
